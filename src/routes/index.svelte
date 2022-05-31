14f495319f245e9d9d57bfcaf58c6b8d99a79758
NickRTR <95316598+NickRTR@users.noreply.github.com>
2022-05-31 17:48:11 +0200

    Basic functionality

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    // use simple-icons package to get the hex code
+
+    let title = "Svelte";
+    let backgroundColor = "000";
+    let color = "fff";
+
+
+    $: url = `https://img.shields.io/badge/${title}-${backgroundColor}?style=for-the-badge&logo=${title}&logoColor=${color}`;
+</script>
+
+<main>
+    <form>
+        <input type="text" placeholder="title" bind:value={title}>
+    </form>
+    <img src={url} alt={title} title={title}>
+</main>
